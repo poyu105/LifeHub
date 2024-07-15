040b2f9bb38ee6344d3e9d0e11a559303239f4b0
poyu <chen06115@gmail.com>
2024-07-15 15:39:59 +0800

    添加navbar自定色

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'custom-navbar':'#F7E7CE'
+      }
+    },
   },
   plugins: [],
 }
