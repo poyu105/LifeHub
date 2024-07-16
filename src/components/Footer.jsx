e9f229a0dcf780754ca235d32f87bb3f3f72297d
poyu <chen06115@gmail.com>
2024-07-17 00:42:02 +0800

    修改顯示問題

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,20 +22,20 @@ export default function Footer() {
 
     return (
     <footer className="bg-black text-zinc-50 text-center mt-3">
-    <div className="py-2 my-3">
-    <small className="lastModifiedDate text-center">Update: {lastModifiedDate}</small><br/>
-    <small>&copy;2024 <a className="text-center hover:text-zinc-300" href="https://poyu105.github.io/myweb" target="_blank">Poyu webDev.</a></small>
-    <span>
-    <a className="m-1 hover:text-zinc-300" href="https://github.com/poyu105" target="_blank"><i className="bi bi-github"></i></a>
-    </span>
-    <span>
-    <a className="m-1 hover:text-zinc-300" href="https://www.instagram.com/ch_03105?igsh=Z2oxNDQ4MHZ5dm53&utm_source=qr" target="_blank"><i className="bi bi-instagram"></i></a>
-    </span>
-    <span>
-    <a className="m-1 hover:text-zinc-300" href="https://www.facebook.com/profile.php?id=100015264487352" target="_blank"><i className="bi bi-facebook"></i></a>
-    </span><br/>
-    <small>本網頁所用之圖片、資訊皆為製作個人作品使用，非商業營利用途，如有冒犯，請郵件: <a className="text-center hover:text-zinc-300" href="mailto:chen06115@gmail.com">chen06115@gmail.com (點擊開啟)</a>，收到後將盡速下架!</small>
-    </div>
+        <div className="py-2">
+            <small className="lastModifiedDate text-center">Update: {lastModifiedDate}</small><br/>
+            <small>&copy;2024 <a className="text-center hover:text-zinc-300" href="https://poyu105.github.io/myweb" target="_blank">Poyu webDev.</a></small>
+            <span>
+                <a className="m-1 hover:text-zinc-300" href="https://github.com/poyu105" target="_blank"><i className="bi bi-github"></i></a>
+            </span>
+            <span>
+                <a className="m-1 hover:text-zinc-300" href="https://www.instagram.com/ch_03105?igsh=Z2oxNDQ4MHZ5dm53&utm_source=qr" target="_blank"><i className="bi bi-instagram"></i></a>
+            </span>
+            <span>
+                <a className="m-1 hover:text-zinc-300" href="https://www.facebook.com/profile.php?id=100015264487352" target="_blank"><i className="bi bi-facebook"></i></a>
+            </span><br/>
+            <small>本網頁所用之圖片、資訊皆為製作個人作品使用，非商業營利用途，如有冒犯，請郵件: <a className="text-center hover:text-zinc-300" href="mailto:chen06115@gmail.com">chen06115@gmail.com (點擊開啟)</a>，收到後將盡速下架!</small>
+        </div>
     </footer>
     );
 }
