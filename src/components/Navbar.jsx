eeb20965fb5f49b723d38f2d1b2a537b37a8d74c
poyu <chen06115@gmail.com>
2024-07-17 01:01:49 +0800

    修正nav-item在mobile寬度填充問題

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,21 +27,21 @@ export default function Navbar(){
                         </button>
                     </div>
                     {/*Main Navigates */}
-                    <ul className={"text-center sm:flex justify-between sm:mx-0 sm:max-h-screen sm:transition-none "+(showMenu ? "max-h-screen transition-max-height duration-500 ease-in-out":"max-h-0 overflow-hidden")}>
+                    <ul className={"text-center flex sm:flex-row flex-col justify-between sm:mx-0 sm:max-h-screen sm:transition-none "+(showMenu ? "max-h-screen transition-max-height duration-500 ease-in-out":"max-h-0 overflow-hidden")}>
                         <li className="my-auto mx-3 hover:border-b-2 hover:border-b-stone-600">
-                            <Link to="/FoodRec">美食推薦</Link>
+                            <Link to="/FoodRec" className="block w-full">美食推薦</Link>
                         </li>
                         <li className="my-auto mx-3 hover:border-b-2 hover:border-b-stone-600">
-                            <Link to="/TrafficGuide">交通指南</Link>
+                            <Link to="/TrafficGuide" className="block w-full">交通指南</Link>
                         </li>
                         <li className="my-auto mx-3 hover:border-b-2 hover:border-b-stone-600">
-                            <Link to="/ShoppingGuide">購物指南</Link>
+                            <Link to="/ShoppingGuide" className="block w-full">購物指南</Link>
                         </li>
                         <li className="my-auto mx-3 hover:border-b-2 hover:border-b-stone-600">
-                            <Link to="/Entertainment">休閒娛樂</Link>
+                            <Link to="/Entertainment" className="block w-full">休閒娛樂</Link>
                         </li>
                         <li className="my-auto mx-3">
-                            <button>
+                            <button className="block w-full">
                                 <i className="bi bi-person-circle"></i>
                                 <span className="sm:hidden inline-block">個人檔案</span>
                             </button>
