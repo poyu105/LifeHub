c0dd1af2e1e4581f2d3fc58b04c0cab425d91353
poyu <chen06115@gmail.com>
2024-07-30 23:42:36 +0800

    修正表單空值驗證功能

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,6 +12,11 @@ export default function Login(){
 
     const handleSubmit = async (e)=>{
         e.preventDefault();
+        //手動檢查表單驗證
+        if (!e.target.checkValidity()) {
+            e.stopPropagation();
+            return;
+        }
         try{
             const response = await axios.post('http://localhost:3001/api/users/login',{
                 email,
@@ -29,20 +34,20 @@ export default function Login(){
     return(
         <>
             <h2 className="text-2xl font-bold text-center mt-3">登入</h2>
-            <form className="md:w-4/12 sm:w-5/12 customer-ssm:w-7/12 customer-ssm:mx-auto mx-2">
+            <form onSubmit={handleSubmit} className="md:w-4/12 sm:w-5/12 customer-ssm:w-7/12 customer-ssm:mx-auto mx-2">
                 <div className="mb-2">
                     <label>
                         <strong>帳號(email):</strong>
                     </label>
-                    <input type="text" placeholder="帳號(email)" value={email} onChange={(e)=>{setEmail(e.target.value)}} className="w-full border-stone-200 border-2"></input>
+                    <input type="text" placeholder="帳號(email)" value={email} onChange={(e)=>{setEmail(e.target.value)}} className="w-full border-stone-200 border-2" required></input>
                 </div>
                 <div className="mb-2">
                     <label>
                         <strong>密碼:</strong>
                     </label>
-                    <input type="password" placeholder="密碼" value={password} onChange={(e)=>{setPassword(e.target.value)}} className="w-full border-stone-200 border-2"></input>
+                    <input type="password" placeholder="密碼" value={password} onChange={(e)=>{setPassword(e.target.value)}} className="w-full border-stone-200 border-2" required></input>
                 </div>
-                <button type="submit" onClick={handleSubmit} className="bg-blue-500 text-white w-full rounded">登入</button>
+                <button type="submit" className="bg-blue-500 text-white w-full rounded">登入</button>
                 <div className="mt-1 text-sm text-center">
                     <span>沒有帳號?&emsp;</span>
                     <Link to='/Register' className=" text-blue-700 border-b-2 border-blue-700 hover:text-blue-400 hover:border-blue-400">註冊</Link>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useState } from "react";
 import Footer from "../components/Footer";
 
-export default function Register(){
+export default function Register() {
 
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
@@ -13,8 +13,13 @@ export default function Register(){
     const [birthDate, setBirthDate] = useState('');
     const navigate = useNavigate();
 
-    const handleSubmit = async (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        //手動檢查表單驗證
+        if (!e.target.checkValidity()) {
+            e.stopPropagation();
+            return;
+        }
         try {
             const response = await axios.post('http://localhost:3001/api/users/register', {
                 username,
@@ -31,53 +36,53 @@ export default function Register(){
         }
     }
 
-    return(
+    return (
         <>
             <h2 className="text-2xl font-bold text-center mt-3">註冊</h2>
-            <form className="md:w-4/12 sm:w-5/12 customer-ssm:w-7/12 customer-ssm:mx-auto mx-2">
-            <div className="mb-2">
+            <form onSubmit={handleSubmit} className="md:w-4/12 sm:w-5/12 customer-ssm:w-7/12 customer-ssm:mx-auto mx-2">
+                <div className="mb-2">
                     <label>
                         <strong>使用者名稱:</strong>
                     </label>
-                    <input type="text" placeholder="使用者名稱" value={username} onChange={(e)=>{setUsername(e.target.value)}} className="w-full border-stone-200 border-2"></input>
+                    <input type="text" placeholder="使用者名稱" value={username} onChange={(e) => { setUsername(e.target.value) }} className="w-full border-stone-200 border-2" required />
                 </div>
                 <div className="mb-2">
                     <label>
                         <strong>Email(帳號):</strong>
                     </label>
-                    <input type="text" placeholder="Email(帳號)" value={email} onChange={(e)=>{setEmail(e.target.value)}} className="w-full border-stone-200 border-2"></input>
+                    <input type="email" placeholder="Email(帳號)" value={email} onChange={(e) => { setEmail(e.target.value) }} className="w-full border-stone-200 border-2" required />
                 </div>
                 <div className="mb-2">
                     <label>
                         <strong>密碼:</strong>
                     </label>
-                    <input type="password" placeholder="密碼" value={password} onChange={(e)=>{setPassword(e.target.value)}} className="w-full border-stone-200 border-2"></input>
+                    <input type="password" placeholder="密碼" value={password} onChange={(e) => { setPassword(e.target.value) }} className="w-full border-stone-200 border-2" required />
                 </div>
                 <div className="mb-2">
                     <label>
                         <strong>確認密碼:</strong>
                     </label>
-                    <input type="password" placeholder="確認密碼" value={confirmPassword} onChange={(e)=>{setConfirmPassword(e.target.value)}} className="w-full border-stone-200 border-2"></input>
+                    <input type="password" placeholder="確認密碼" value={confirmPassword} onChange={(e) => { setConfirmPassword(e.target.value) }} className="w-full border-stone-200 border-2" required />
                 </div>
                 <div className="mb-2">
                     <label>
                         <strong>電話:</strong>
                     </label>
-                    <input type="number" placeholder="電話" value={phoneNumber} onChange={(e)=>{setPhoneNumber(e.target.value)}} className="w-full border-stone-200 border-2 appearance-none"></input>
+                    <input type="number" placeholder="電話" value={phoneNumber} onChange={(e) => { setPhoneNumber(e.target.value) }} className="w-full border-stone-200 border-2 appearance-none" required />
                 </div>
                 <div className="mb-2">
                     <label>
                         <strong>生日:</strong>
                     </label>
-                    <input type="date" value={birthDate} onChange={(e)=>{setBirthDate(e.target.value)}} className="w-full border-stone-200 border-2"></input>
+                    <input type="date" value={birthDate} onChange={(e) => { setBirthDate(e.target.value) }} className="w-full border-stone-200 border-2" required />
                 </div>
-                <button type="submit" onClick={handleSubmit} className="bg-blue-500 text-white w-full rounded">註冊</button>
+                <button type="submit" className="bg-blue-500 text-white w-full rounded">註冊</button>
                 <div className="mt-1 text-sm text-center">
                     <span>已經有帳號?&emsp;</span>
                     <Link to='/Login' className=" text-blue-700 border-b-2 border-blue-700 hover:text-blue-400 hover:border-blue-400">登入</Link>
                 </div>
             </form>
-            <Footer/>
+            <Footer />
         </>
     )
 }
