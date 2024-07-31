f584151f374b583e20b73f91689837562a6555cc
poyu <chen06115@gmail.com>
2024-07-31 13:15:20 +0800

    新增測試資料

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useState, useContext } from 'react';
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState({username: '測試用名稱', id:'測試用ID_AABCD00123_test', email: 'testAAA@gmail.com'});
 
     const login = (userData) => {
         setUser(userData);
