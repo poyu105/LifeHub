3aa3cc6ea3ffc9e341d1fa712299888df0d07235
poyu <chen06115@gmail.com>
2024-10-03 18:56:38 +0800

    新增顯示用戶貼文功能

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { json } from "react-router-dom";
+import axios from "axios";
 export default function Profile() {
     // 獲取當前用戶
     const { user, logout, updateUser } = useAuth();
@@ -8,6 +8,8 @@ export default function Profile() {
     const [email, setEmail] = useState(user.email);
     const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber);
     const [birthDate, setBirthDate] = useState(user.birthDate);
+    //用戶貼文
+    const [posts, setPosts] = useState([]);
     //表單編輯狀態
     const [isEditable, setIsEditable] = useState(false);
     //格式化日期
@@ -59,6 +61,24 @@ export default function Profile() {
             handleCancel(e);
         }
     }
+    //獲取用戶貼文
+    const fetchPosts = async ()=>{
+        try {
+            const response = await axios.get(`http://localhost:3001/api/users/${user.id}/posts`);
+            if(response.status==200){
+                setPosts(response.data);
+            }
+        } catch (error) {
+            setPosts({state: 'ERROR', message: error.message});
+            console.log(`載入用戶貼文失敗! ${error}`);
+            alert('載入用戶貼文失敗!');
+        }
+    }
+    useEffect(()=>{
+        if(user){
+            fetchPosts();
+        }
+    },[user]);
 
     return (
         <>
@@ -75,7 +95,7 @@ export default function Profile() {
                         </header>
                         <hr/>
                         {/* body */}
-                        <body className="flex justify-center flex-col items-center">
+                        <body className="flex justify-center flex-col items-center mb-4">
                             <div className="flex justify-evenly items-center my-2 w-full">
                                 <div className="rounded-full w-fit">
                                     <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" className="border-3 border-slate-300 rounded-full w-40"/>
@@ -105,17 +125,46 @@ export default function Profile() {
                                             </>
                                         ):(
                                             <> 
-                                                <button onClick={(e)=> {e.preventDefault(); setIsEditable(true);}} className="bg-yellow-300 hover:bg-yellow-400 text-black p-1 mt-2 rounded">編輯</button>
+                                                <button onClick={(e)=> {e.preventDefault(); setIsEditable(true);}} className="bg-yellow-300 hover:bg-yellow-400 text-black p-1 mt-2 rounded">編輯個人資料</button>
                                             </>
                                         )}
                                     </form>
                                     <div className="w-1/2 text-center">
                                         <h1 className="text-3xl font-bold">貼文數</h1>
-                                        <p className="text-2xl">50</p>
+                                        <p className="text-2xl">{posts.state==='ERROR'? 'ERROR':posts.length}</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="bg-slate-200 w-4/5 h-0.5 rounded"></div>
+                            {posts.state === 'ERROR' ? (
+                                <div>
+                                    <h2 className="text-xl font-bold">貼文載入失敗:</h2>
+                                    <p>{posts.message}</p>
+                                </div>
+                            ): (
+                                posts.length >0 ? (
+                                    <div className="grid md:grid-cols-4 md:gap-4 grid-cols-2 gap-2 w-4/5 my-2">
+                                        {posts.map((post)=>{
+                                            return(
+                                                <div key={post._id} className="overflow-hidden rounded">
+                                                    {post.mediaFiles[0].type == 'image' ? (
+                                                        <img className="w-full h-auto aspect-square object-cover rounded" src={'http://localhost:3001/'+post.mediaFiles[0].url} alt="postMedia"></img>
+                                                    ):(
+                                                        <video className="w-full h-auto aspect-square object-cover rounded">
+                                                            <source src={'http://localhost:3001/'+post.mediaFiles[0].url} type="video/mp4"></source>
+                                                        </video>
+                                                    )}
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                ):(
+                                    <div>
+                                        <h2 className="font-bold text-xl">目前沒有貼文喔!</h2>
+                                    </div>
+                                )
+                            )
+                            }
                         </body>
                     </div>
                 </>
